f75467cd6c522d75eded7a5e52ed4adf9171e134
qkrgusrl <108668603+qkrgusrl@users.noreply.github.com>
2022-07-25 17:32:28 +0900

    for8.c

---
+++ 7_25/baekjoon/for8.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main(){
+    int T, a, b, i;
+
+    scanf("%d", &T);
+
+    for(i = 1; i <= T; i++){
+        scanf("%d %d", &a, &b);
+
+        if (a < 0 || b >= 10){
+            break;
+        }
+        
+
+        printf("Case #%d: %d + %d = %d\n", i, a, b, a + b);
+    }
+
+    return 0;
+}
